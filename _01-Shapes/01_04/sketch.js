798825264f74e268e51c94142578c7929567deb2
tillou <till.loetscher@stud.hslu.ch>
2019-12-16 21:55:56 +0100

    cleanup2

--- _01-Shapes/01_04/sketch.js
+++ _01-Shapes/01_04/sketch.js
@@ -65,3 +65,16 @@ class snowflake {
     fill(230,21,145);
   }
 }
+
+
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveThumb(650, 350);
+}
+
+// Thumb
+function saveThumb(w, h) {
+  let img = get( width/2-w/2, height/2-h/2, w, h);
+  save(img,'thumb.jpg');
+}
+
